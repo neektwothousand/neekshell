66252dd2044cee1ce9e49d91b3c9b1a03906b5d8
neektwothousand <33990922+neektwothousand@users.noreply.github.com>
2020-03-24 08:14:28 +0100

    fixed !bin & general revision

--- neekshellbot/neekshellbot.sh
+++ neekshellbot/neekshellbot.sh
@@ -136,8 +136,7 @@ function inline_tag() {
 EOF
 }
 function get_feedback_reply() {
-	update=$1
-	first_normal=$(echo $text | sed "s/@$(cat ./botinfo | jq ".result.username" | sed "s/\"//g")//")
+	first_normal=$(sed "s/@$(jq ".result.username" botinfo | sed "s/\"//g")//" <<< $text)
 	normaldice=$(echo $first_normal | tr -d '/![:alpha:]')
 	inlinedice=$(echo $results | tr -d '[:alpha:]')
 	return_query=$(inline_help)
@@ -149,18 +148,17 @@ function get_feedback_reply() {
 			return_feedback=$(echo -e "!d[number] (dice)\n!fortune (fortune cookie)\n!owoifer (on reply)\n!sed [regexp] (on reply)\n!forward [usertag] (in private, on reply)\n!tag [[@username] (new tag text)] (in private)\n!ping\n\nadministrative commands:\n\n!bin [system command]\n!setadmin @username\n!deladmin @username\n!bang (on reply to mute)\n\ninline mode:\n\nd[number] (dice)\n[system command] bin\ntag [[@username] (new tag text)]\nsearch [text to search on google]\ngel [gelbooru tag]\nxbgif [xbooru gif tag]")
 		;;
 		"!d$normaldice"|"/d$normaldice")
-			number=$(( ( RANDOM % $normaldice )  + 1 ))
-			return_feedback=$(echo $number)
+			return_feedback=$(echo $(( ( RANDOM % $normaldice )  + 1 )))
 		;;
 		"!setadmin "*|"/setadmin "*)
-			admin=$(cat neekshelladmins | grep -v "#" | grep -w $username_id)
-			if [ "x$admin" != "x"  ]; then
-				username=$(echo $first_normal | sed -e 's/[/!]setadmin @//')
-				setadmin_id=$(cat ./neekshell_db/users/$username | sed 's/\s.*$//')
-				admin_check=$(cat neekshelladmins | grep -v "#" | grep -w $setadmin_id)
+			admin=$(grep -v "#" neekshelladmins | grep -w $username_id)
+			if [ "$admin" != ""  ]; then
+				username=$(sed -e 's/[/!]setadmin @//' <<< $first_normal)
+				setadmin_id=$(sed 's/\s.*$//' neekshell_db/users/$username)
+				admin_check=$(grep -v "#" neekshelladmins | grep -w $setadmin_id)
 				if [ -z $setadmin_id ]; then
 					return_feedback=$(echo "user not found")
-				elif [ "x$admin_check" != "x" ]; then
+				elif [ "$admin_check" != "" ]; then
 					return_feedback=$(echo "$username already admin")
 				else
 					echo -e "# $username\n$setadmin_id" >> neekshelladmins
@@ -171,11 +169,11 @@ function get_feedback_reply() {
 			fi
 		;;
 		"!deladmin "*|"/deladmin "*)
-			admin=$(cat neekshelladmins | grep -v "#" | grep -w $username_id)
-			if [ "x$admin" != "x"  ]; then
-				username=$(echo $first_normal | sed -e 's/[/!]deladmin @//')
-				deladmin_id=$(cat ./neekshell_db/users/$username | sed 's/\s.*$//')
-				admin_check=$(cat neekshelladmins | grep -v "#" | grep -w $deladmin_id)
+			admin=$(grep -v "#" neekshelladmins | grep -w $username_id)
+			if [ "$admin" != ""  ]; then
+				username=$(sed -e 's/[/!]deladmin @//' <<< $first_normal)
+				deladmin_id=$(sed 's/\s.*$//' neekshell_db/users/$username)
+				admin_check=$(grep -v "#" neekshelladmins | grep -w $deladmin_id)
 				if [ -z $deladmin_id ]; then
 					return_feedback=$(echo "user not found")
 				elif [ "x$admin_check" != "x" ]; then
@@ -190,11 +188,11 @@ function get_feedback_reply() {
 			fi
 		;;
 		"!bin "*|"/bin "*)
-			admin=$(cat neekshelladmins | grep -v "#" | grep -w $username_id)
-			if [ "x$admin" != "x" ]
+			admin=$(grep -v "#" neekshelladmins | grep -w $username_id)
+			if [ "$admin" != "" ]
 				then
-				command="echo \$($(echo $update | jq -r ".message.text" | sed -e 's/[/!]bin//'))"
-				return_feedback="<code>$(eval $(echo "timeout 2s $command" ) 2>&1 )</code>"
+				command="echo \$($(jq -r ".message.text" tempinput | sed -e 's/[/!]bin//'))"
+				return_feedback="$(eval $(echo "timeout 2s $(echo $command)") 2>&1 )"
 				else
 				return_feedback="<code>Access denied</code>"
 			fi
@@ -203,54 +201,48 @@ function get_feedback_reply() {
 			return_feedback="$(/usr/games/fortune fortunes paradoxum goedel linuxcookie)"
 		;;
 		"!owoifer"|"/owoifer"|"!cringe"|"/cringe")
-			reply=$(echo $update | jq -r ".message.reply_to_message.text")
+			reply=$(jq -r ".message.reply_to_message.text" tempinput)
 			if [ "$reply" != "null" ]; then
-				if	[ $(echo $first_normal | sed 's/[!/]//') = "cringe" ]; then
-					owoarray=(" 🥵 " " 🙈 " " 🤣 " " 😘 " " 🥺 " " 💁‍♀️ " " OwO " " 😳 " " 🤠 " " 🤪 " " 😜 " " 🤬 " " 🤧 " " 🦹‍♂ ")
-				else
-					owoarray=(" owo " " ewe " " uwu ")
-				fi
-				numberspace=$(echo $reply | sed 's/ / \n/g' | grep -c " ")
-				number=$(echo "$numberspace / 3" | bc)
-				resultspace=$(echo -e "$number" && echo -e "\n$number + $number" | bc && echo -e "\n$number*3" | bc)
-				tempspace=$(echo $resultspace | sed -e "s/\s/\n/g")
+				[ $(sed 's/[!/]//' <<< $first_normal) = "cringe" ] && owoarray=(" 🥵 " " 🙈 " " 🤣 " " 😘 " " 🥺 " " 💁‍♀️ " " OwO " " 😳 " " 🤠 " " 🤪 " " 😜 " " 🤬 " " 🤧 " " 🦹‍♂ ") || owoarray=(" owo " " ewe " " uwu ")
+				numberspace=$(sed 's/ / \n/g' <<< $reply | grep -c " ")
+				number=$(bc <<< "$numberspace / 3")
+				resultspace=$(echo "$number" ; bc <<< "$number + $number" ; bc <<< "$number*3")
+				tempspace=$(sed -e "s/\s/\n/g" <<< $resultspace)
 				x=0
 				rig=1
 				while [ $x -le "2" ]; do
-					spacerandom[$x]=$(echo -e "$tempspace" | sed -n "${rig}p")
+					spacerandom[$x]=$(sed -n "${rig}p" <<< $tempspace)
 					cringerandom[$x]=${owoarray[$(( ( RANDOM % ${#owoarray[@]} )  + 0 ))]}
 					rig=$(( $rig + 1 ))
 					x=$(( $x + 1 ))
 				done
-				emoji=$(echo $reply | sed -e "s/ /${cringerandom[0]}/${spacerandom[0]}" -e "s/ /${cringerandom[1]}/${spacerandom[1]}" -e "s/ /${cringerandom[2]}/${spacerandom[2]}")
-				owo=$(echo $emoji | sed -e 's/[lr]/w/g' -e 's/[LR]/W/g')
-				return_feedback=$(echo $owo)
+				emoji=$(sed -e "s/ /${cringerandom[0]}/${spacerandom[0]}" -e "s/ /${cringerandom[1]}/${spacerandom[1]}" -e "s/ /${cringerandom[2]}/${spacerandom[2]}" <<< $reply)
+				return_feedback=$(sed -e 's/[lr]/w/g' -e 's/[LR]/W/g' <<< $emoji)
 			else
 				exit 1
 			fi
 		;;
 		"!sed "*|"/sed "*)
-			regex=$(echo $first_normal | sed -e 's/[/!]sed //')
-			reply=$(echo $update | jq -r ".message.reply_to_message.text")
-			sed=$(echo "$reply" | sed -E "$regex")
+			regex=$(sed -e 's/[/!]sed //' <<< $first_normal)
+			sed=$(jq -r ".message.reply_to_message.text" tempinput | sed -E "$regex")
 			return_feedback=$(echo -e "<b>FTFY:</b>\n$sed")
 		;;
 		"!ping"|"/ping")
 			end=`date +%s%2N`
-			runtime=$( echo "$end - $start" | bc -l )
+			runtime=$(bc -l <<< "$end - $start")
 			return_feedback=$(echo -e "<b>pong</b>\n$SECONDS.${runtime}s")
 		;;
 		"!bang"|"/bang")
 		if [ $type != "private" ]; then
-			admin=$(cat neekshelladmins | grep "$normal_user")
-			if [ -n "$admin" ]
+			admin=$(grep -v "#" neekshelladmins | grep -w $username_id)
+			if [ "$admin" != "" ]
 				then
-				usertag=$(echo $update | jq -r ".message.reply_to_message.from.username")
-				userid=$(cat ./neekshell_db/users/$usertag | sed 's/\s.*$//')
-				ban=$(wget -q -O/dev/null --post-data "chat_id=$chat_id&user_id=$userid&can_send_messages=false&until_date=32477736097" "${TELEAPI}/restrictChatMember" & wget --post-data "chat_id=$chat_id&reply_to_message_id=$message_id&parse_mode=html&text=$(echo -e "<b>boom</b>\nutente @$usertag (<a href=\"tg://user?id=$userid\">$userid</a>) terminato")" "${TELEAPI}/sendMessage")
+					usertag=$(jq -r ".message.reply_to_message.from.username" tempinput)
+					userid=$(sed 's/\s.*$//' neekshell_db/users/$usertag)
+					ban=$(wget -q -O/dev/null --post-data "chat_id=$chat_id&user_id=$userid&can_send_messages=false&until_date=32477736097" "${TELEAPI}/restrictChatMember" & wget --post-data "chat_id=$chat_id&reply_to_message_id=$message_id&parse_mode=html&text=$(echo -e "<b>boom</b>\nutente @$usertag (<a href=\"tg://user?id=$userid\">$userid</a>) terminato")" "${TELEAPI}/sendMessage")
 				exit 1
 				else
-				return_denied=$(wget -q -O/dev/null --post-data "chat_id=$chat_id&parse_mode=html&text=<code>Access denied</code>" "${TELEAPI}/sendMessage")
+					return_denied=$(wget -q -O/dev/null --post-data "chat_id=$chat_id&parse_mode=html&text=<code>Access denied</code>" "${TELEAPI}/sendMessage")
 				exit 1
 			fi
 		else
@@ -259,9 +251,9 @@ function get_feedback_reply() {
 		;;
 		"!tag "*|"/tag "*)
 		if [ $type = "private" ]; then
-			usertag=$(echo $first_normal | sed -Ee 's/(.* )(\[.*\]) ((.*))/\2/' -e 's/[[:punct:]]//g')
-			usertext=$(echo $first_normal | sed -Ee 's/(.* )(\[.*\]) ((.*))/\3/' -Ee 's/[[:punct:]](.*)[[:punct:]]/\1/')
-			userid=$(cat ./neekshell_db/users/$(echo $usertag) | sed 's/\s.*$//')
+			usertag=$(sed -Ee 's/(.* )(\[.*\]) ((.*))/\2/' -e 's/[[:punct:]]//g' <<< $first_normal)
+			usertext=$(sed -Ee 's/(.* )(\[.*\]) ((.*))/\3/' -Ee 's/[[:punct:]](.*)[[:punct:]]/\1/' <<< $first_normal)
+			userid=$(sed 's/\s.*$//' neekshell_db/users/$usertag)
 			return_feedback=$(echo -e "<a href=\"tg://user?id=$userid\">$usertext</a>")
 		else
 			exit 1
@@ -270,7 +262,7 @@ function get_feedback_reply() {
 		"!forward "*|"/forward "*)
 		if [ $type = "private" ]; then
 			username=$(echo $first_normal | sed -e 's/[/!]forward @//')
-			forward_id=$(cat ./neekshell_db/users/$username | sed 's/\s.*$//')
+			forward_id=$(sed 's/\s.*$//' neekshell_db/users/$username)
 			forward=$(wget -q -O/dev/null --post-data "chat_id=$forward_id&from_chat_id=$chat_id&message_id=$message_id" "${TELEAPI}/forwardMessage" )
 			return_feedback="Inviato"
 		else
@@ -280,26 +272,23 @@ function get_feedback_reply() {
 		*)
 		if [ $type = "private" ]; then
 			number=$(( ( RANDOM % 500 )  + 1 ))
-			if	[ $number = 69 ]
-				then
-				reply="Nice."
-			elif	[ $number = 1 ]
-				then
-				reply="Sei il number uno"
-			elif	[ $number -gt 250 ]
-				then
-				reply="Ok"
-			elif	[ $number -lt 250 ]
-				then
-				reply="Alright"
+			if		[ $number = 69 ]; then
+				return_feedback="Nice."
+			elif	[ $number = 1 ]; then
+				return_feedback="We are number one"
+			elif	[ $number -gt 250 ]; then
+				return_feedback="Ok"
+			elif	[ $number -lt 250 ]; then
+				return_feedback="Alright"
 			fi
-			return_feedback=$reply
+		else
+			exit 1
 		fi
 		;;
 	esac
     case $results in
         "help")
-            	return_query=$(inline_help)
+            return_query=$(inline_help)
         ;;
 		"d$inlinedice")
 		if [ -n "$inlinedice" ]
@@ -309,32 +298,27 @@ function get_feedback_reply() {
 		fi
 		;;
         *" bin")
-		admin=$(cat neekshelladmins | grep -v "#" | grep -w $inline_user_id)
-		    if [ "x$admin" != "x" ]
-			then
-                	command="echo \$($(echo $update | jq -r ".inline_query.query" | sed -e 's/ bin//'))"
-                	command_result=$(eval $(echo "timeout 2s $(echo $command)") 2>&1 )
-			return_query=$(inline_command)
-			else
-			return_query=$(inline_denied)
-		    fi
-        ;;
-	"tag "*)
-		usertag=$(echo $results | sed -Ee 's/(.* )(\[.*\]) ((.*))/\2/' -e 's/[[:punct:]]//g')
-		usertext=$(echo $results | sed -Ee 's/(.* )(\[.*\]) ((.*))/\3/' -Ee 's/[[:punct:]](.*)[[:punct:]]/\1/')
-		userid=$(cat ./neekshell_db/users/$(echo $usertag | sed 's/@//') | sed 's/\s.*$//')
-		return_query=$(inline_tag)
-	;;
-        'search '*)
-            offset=$(($(echo $update | jq -r ".inline_query.offset")+1))
-            search=$(echo $results | sed 's/search //g')
-            resnumber=$(googler --unfilter --json -n 5 -s $offset $search | jq -r ". | length")
-            if [ $offset = 1 ]
+			admin=$(grep -v "#" neekshelladmins | grep -w $username_id)
+            if [ "$admin" != "" ]
                 then
-                nextpage=1
+                command="echo \$($(jq -r ".inline_query.query" tempinput | sed -e 's/ bin//'))"
+                command_result=$(eval $(echo "timeout 2s $(echo $command)") 2>&1 )
+                return_query=$(inline_command)
                 else
-                nextpage=$(($(echo $offset)+$(echo $resnumber)))
+                return_query=$(inline_denied)
             fi
+        ;;
+		"tag "*)
+			usertag=$(sed -Ee 's/(.* )(\[.*\]) ((.*))/\2/' -e 's/[[:punct:]]//g' <<< $results)
+			usertext=$(sed -Ee 's/(.* )(\[.*\]) ((.*))/\3/' -Ee 's/[[:punct:]](.*)[[:punct:]]/\1/' <<< $results)
+			userid=$(sed 's/\s.*$//' neekshell_db/users/$(sed 's/@//' <<< $usertag))
+			return_query=$(inline_tag)
+		;;
+        'search '*)
+            offset=$(($(jq -r ".inline_query.offset" tempinput)+1))
+            search=$(sed 's/search //g' <<< $results)
+            resnumber=$(googler --unfilter --json -n 5 -s $offset $search | jq -r ". | length")
+            [ $offset = 1 ] && nextpage=1 || nextpage=$(($offset+$resnumber))
             googler_results=$(googler --unfilter --json -n 5 -s $nextpage $search)
 			x=0
             return_query=$(inline_google)
@@ -342,59 +326,53 @@ function get_feedback_reply() {
         'gel '*)
             el=0
             rig=1
-            offset=$(($(echo $update | jq -r ".inline_query.offset")+1))
-            tags=$(echo $results | sed 's/gel //g')
+            offset=$(($(jq -r ".inline_query.offset" tempinput)+1))
+            tags=$(sed 's/gel //g' <<< $results)
 			wget --user-agent 'Mozilla/5.0' -qO - "https://gelbooru.com/index.php?page=dapi&s=post&pid=$offset&tags=$tags&q=index&limit=20&api_key=20169d287696c16948e85c8ee715241ea636662e7419186d21f3355b4432c5ba&user_id=455640" > tempgel
-            thumblist=$(cat tempgel | sed -n 's/.*preview_url="\([^"]*\)".*/\1/p' | grep -E 'jpg|jpeg|png')
-            piclist=$(cat tempgel | sed -n 's/.*sample_url="\([^"]*\)".*/\1/p' | grep -E 'jpg|jpeg|png')
-            filelist=$(cat tempgel | sed -n 's/.*file_url="\([^"]*\)".*/\1/p' | grep -E 'jpg|jpeg|png')
-            picnumber=$(cat tempgel | sed -n 's/.*sample_url="\([^"]*\)".*/\1/p' | grep -E -c 'jpg|jpeg|png')
+            thumblist=$(sed -n 's/.*preview_url="\([^"]*\)".*/\1/p' tempgel | grep -E 'jpg|jpeg|png')
+            piclist=$(sed -n 's/.*sample_url="\([^"]*\)".*/\1/p' tempgel | grep -E 'jpg|jpeg|png')
+            filelist=$(sed -n 's/.*file_url="\([^"]*\)".*/\1/p' tempgel | grep -E 'jpg|jpeg|png')
+            picnumber=$(sed -n 's/.*sample_url="\([^"]*\)".*/\1/p' tempgel | grep -E -c 'jpg|jpeg|png')
             return_query=$(inline_booru)
-            rm tempgel
         ;;
         'xb '*)
             el=0
             rig=1
-            offset=$(($(echo $update | jq -r ".inline_query.offset")+1))
-            tags=$(echo $results | sed 's/xb //g')
+            offset=$(($(jq -r ".inline_query.offset" tempinput)+1))
+            tags=$(sed 's/xb //g' <<< $results)
             wget --user-agent 'Mozilla/5.0' -qO - "https://xbooru.com/index.php?page=dapi&s=post&pid=$offset&tags=$tags&q=index&limit=20" > tempxb
-            thumblist=$(cat tempxb | sed -n 's/.*preview_url="\([^"]*\)".*/\1/p' | grep -E 'jpg|jpeg|png')
-            piclist=$(cat tempxb | sed -n 's/.*sample_url="\([^"]*\)".*/\1/p' | grep -E 'jpg|jpeg|png')
-            filelist=$(cat tempxb | sed -n 's/.*file_url="\([^"]*\)".*/\1/p' | grep -E 'jpg|jpeg|png')
-            picnumber=$(cat tempxb | sed -n 's/.*sample_url="\([^"]*\)".*/\1/p' | grep -E -c 'jpg|jpeg|png')
+            thumblist=$(sed -n 's/.*preview_url="\([^"]*\)".*/\1/p' tempxb | grep -E 'jpg|jpeg|png')
+            piclist=$(sed -n 's/.*sample_url="\([^"]*\)".*/\1/p' tempxb | grep -E 'jpg|jpeg|png')
+            filelist=$(sed -n 's/.*file_url="\([^"]*\)".*/\1/p' tempxb | grep -E 'jpg|jpeg|png')
+            picnumber=$(sed -n 's/.*sample_url="\([^"]*\)".*/\1/p' tempxb | grep -E -c 'jpg|jpeg|png')
             return_query=$(inline_booru)
-            rm tempxb
         ;;
         'xbgif '*)
             el=0
             rig=1
-            offset=$(($(echo $update | jq -r ".inline_query.offset")+1))
-            tags=$(echo $results | sed 's/xbgif //g')
+            offset=$(($(jq -r ".inline_query.offset" tempinput)+1))
+            tags=$(sed 's/xbgif //g' <<< $results)
             wget --user-agent 'Mozilla/5.0' -qO - "https://xbooru.com/index.php?page=dapi&s=post&pid=$offset&tags=gif+$tags&q=index&limit=20" > tempxbgif
-            giflist=$(cat tempxbgif | sed -n 's/.*sample_url="\([^"]*\)".*/\1/p' | grep -E 'gif')
-            filelist=$(cat tempxbgif | sed -n 's/.*file_url="\([^"]*\)".*/\1/p' | grep -E 'gif')
-            gifnumber=$(cat tempxbgif | sed -n 's/.*sample_url="\([^"]*\)".*/\1/p' | grep -E -c 'gif')
+            giflist=$(sed -n 's/.*sample_url="\([^"]*\)".*/\1/p' tempxbgif | grep -E 'gif')
+            filelist=$(sed -n 's/.*file_url="\([^"]*\)".*/\1/p' tempxbgif | grep -E 'gif')
+            gifnumber=$(sed -n 's/.*sample_url="\([^"]*\)".*/\1/p' tempxbgif | grep -E -c 'gif')
             return_query=$(inline_boorugif)
-            rm tempxbgif
         ;;
     esac
 }
 
 function process_reply() {
-	update=$(cat ./tempinput)
-    message_id=$(echo $update | jq -r ".message.reply_to_message.message_id")
-    if [ "$message_id" = "null" ]; then
-	    message_id=$(echo $update | jq -r ".message.message_id")
-    fi
-    normal_user=$(echo $update | jq -r ".message.from.username")
-    username_id=$(echo $update | jq -r ".message.from.id")
-    chat_id=$(echo $update | jq -r ".message.chat.id")
-    inline_user=$(echo $update | jq -r ".inline_query.from.username")
-    inline_user_id=$(echo $update | jq -r ".inline_query.from.id")
-    inline_id=$(echo $update | jq -r ".inline_query.id")
-    type=$(echo $update | jq -r ".message.chat.type")
-	text=$(echo $update | jq -r ".message.text" | sed 's/"/\\&/g')
-	results=$(echo $update | jq -r ".inline_query.query" | sed 's/"/\\&/g')
+    message_id=$(jq -r ".message.reply_to_message.message_id" tempinput)
+    [ "$message_id" = "null" ] && message_id=$(jq -r ".message.message_id" tempinput)
+    normal_user=$(jq -r ".message.from.username" tempinput)
+    username_id=$(jq -r ".message.from.id" tempinput)
+    chat_id=$(jq -r ".message.chat.id" tempinput)
+    inline_user=$(jq -r ".inline_query.from.username" tempinput)
+    inline_user_id=$(jq -r ".inline_query.from.id" tempinput)
+    inline_id=$(jq -r ".inline_query.id" tempinput)
+    type=$(jq -r ".message.chat.type" tempinput)
+	text=$(jq -r ".message.text" tempinput | sed 's/"/\\&/g')
+	results=$(jq -r ".inline_query.query" tempinput | sed 's/"/\\&/g')
 	# database id
 	if [ -n $normal_user ]; then
 		if [ ! -d ./neekshell_db/users/ ]; then
@@ -410,20 +388,15 @@ function process_reply() {
 		touch ./botinfo
 		wget -q -O ./botinfo "${TELEAPI}/getMe"
 	fi
-	get_feedback_reply "$update"
-    if      [ "$first_normal" != "null" ] && [ -n "$first_normal" ]
-        then
+		get_feedback_reply
+    if      [ "$first_normal" != "null" ] && [ -n "$first_normal" ]; then
             result=$(wget -q -O/dev/null --post-data "chat_id=$chat_id&reply_to_message_id=$message_id&parse_mode=html&text=$return_feedback" "${TELEAPI}/sendMessage")
-    elif    [ "$results" != "null" ] && [ -n "$results" ]
-        then
+    elif    [ "$results" != "null" ] && [ -n "$results" ]; then
 			result=$(wget -q -O/dev/null --post-data "inline_query_id=$inline_id&results=$return_query&next_offset=$offset&cache_time=100&is_personal=true" "${TELEAPI}/answerInlineQuery")
     fi
-    if      [ "$first_normal" != "null" ] && [ -n "$first_normal" ] && [ "$type" = "private" ]
-        then
+    if      [ "$first_normal" != "null" ] && [ -n "$first_normal" ] && [ "$type" = "private" ]; then
                 echo -e "\n--\nnormal=${text}\nfrom ${normal_user} at $(date "+%Y-%m-%d %H:%M")\n--"
-        
-    elif    [ "$results" != "null" ] && [ -n "$results" ]
-        then
+    elif    [ "$results" != "null" ] && [ -n "$results" ]; then
                 echo -e "\n--\ninline=${results}\nfrom ${inline_user} at $(date "+%Y-%m-%d %H:%M")\n--"
     fi
     ## activate to log debug
@@ -431,4 +404,5 @@ function process_reply() {
     #echo -e ": inline_query=${return_query}"
     #echo -e ": inline_message=${results}"
 }
+
 process_reply
