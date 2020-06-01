1219ac43c27393becefda1a8cf8a0abd99712720
neektwothousand <craftmallus@gmail.com>
2020-06-01 17:49:44 +0200

    changed dir to php

--- neekshellbot-webhook.php
+++ neekshellbot-webhook.php
@@ -5,6 +5,6 @@ $input = str_replace('\\\\', '\\\\\\', $input);
 $input = str_replace('"', '\"', $input );
 $input = str_replace('$', '\$', $input );
 $input = str_replace('\\\\"', '\\\\\\"', $input);
-$command = "sudo su neekshellbot -c \"cd ./neekshellbot/ && timeout 240s ./neekshellbot.sh $input\"";
+$command = "sudo su neekshellbot -c \"timeout 240s ./neekshellbot.sh $input\"";
 shell_exec ($command);
 ?>
