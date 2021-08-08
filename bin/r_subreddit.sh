72f9d27a6c854a20c4e4423ba2a5109d63f72b7a
NeekTwoThousand <craftmallus@gmail.com>
2021-08-08 13:19:44 +0200

    reddit video support

--- bin/r_subreddit.sh
+++ bin/r_subreddit.sh
@@ -56,12 +56,17 @@ case "$subreddit" in
 	;;
 esac
 media_id=$(jshon -e url -u <<< "$hot" | grep "i.redd.it\|imgur\|gfycat\|redgifs")
-if [[ "$(grep "gfycat" <<< "$media_id")" != "" ]]; then
-	media_id=$(curl -s "$media_id" | sed -En 's|.*<source src="(https://thumbs.*mp4)" .*|\1|p')
-elif [[ "$(grep "redgifs" <<< "$media_id")" != "" ]]; then
-	media_id=$(wget -q -O- "$media_id" | sed -En 's|.*content="(https://thumbs2.redgifs.*-mobile.mp4)".*|\1|p')
-fi
+video=$(jshon -Q -e secure_media -e reddit_video <<< "$hot")
 permalink=$(jshon -e permalink -u <<< "$hot" | cut -f 5 -d /)
+if [[ "$media_id" ]]; then
+	if [[ "$(grep "gfycat" <<< "$media_id")" != "" ]]; then
+		media_id=$(curl -s "$media_id" | sed -En 's|.*<source src="(https://thumbs.*mp4)" .*|\1|p')
+	elif [[ "$(grep "redgifs" <<< "$media_id")" != "" ]]; then
+		media_id=$(wget -q -O- "$media_id" | sed -En 's|.*content="(https://thumbs2.redgifs.*-mobile.mp4)".*|\1|p')
+	fi
+elif [[ "$video" ]]; then
+	youtube-dl --quiet --no-warnings --merge-output-format mp4 -o "$permalink.mp4" "https://www.reddit.com/$permalink/"
+fi
 title=$(jshon -e title -u <<< "$hot")
 stickied=$(jshon -e stickied -u <<< "$hot")
 if [[ "$title" != "" ]]; then
@@ -69,31 +74,35 @@ if [[ "$title" != "" ]]; then
 		"$title" \
 		"link: redd.it/$permalink")
 fi
-if [[ "$media_id" = "" ]]; then
-	text_id=$caption
-	tg_method send_message > /dev/null
-elif [[ "$(grep "jpg\|png" <<< "$media_id")" != "" ]]; then
+if [[ "$(grep "jpg\|png" <<< "$media_id")" != "" ]]; then
 	photo_id=$media_id
 	if [[ "$(tg_method send_photo | jshon -Q -e ok)" = "false" ]]; then
 		text_id=$caption
-		tg_method send_message > /dev/null
+		tg_method send_message
 	fi
 elif [[ "$(grep "gif" <<< "$media_id")" != "" ]]; then
 	animation_id=$media_id
 	if [[ "$(tg_method send_animation | jshon -Q -e ok)" = "false" ]]; then
 		text_id=$caption
-		tg_method send_message > /dev/null
+		tg_method send_message
 	fi
 elif [[ "$(grep "mp4" <<< "$media_id")" != "" ]] && [[ "$(ffprobe "$media_id" 2>&1 | grep -o 'Audio:')" = "" ]]; then
 	animation_id=$media_id
 	if [[ "$(tg_method send_animation | jshon -Q -e ok)" = "false" ]]; then
 		text_id=$caption
-		tg_method send_message > /dev/null
+		tg_method send_message
 	fi
 elif [[ "$(grep "mp4" <<< "$media_id")" != "" ]] && [[ "$(ffprobe "$media_id" 2>&1 | grep -o 'Audio:')" != "" ]]; then
 	video_id=$media_id
 	if [[ "$(tg_method send_video | jshon -Q -e ok)" = "false" ]]; then
 		text_id=$caption
-		tg_method send_message > /dev/null
+		tg_method send_message
 	fi
+elif [[ "$video" ]]; then
+	video_id="@$permalink.mp4"
+	tg_method send_video upload
+	rm -f "$permalink.mp4"
+else
+	text_id=$caption
+	tg_method send_message
 fi
