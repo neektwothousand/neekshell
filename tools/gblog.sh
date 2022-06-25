cbad66d84d1f0a87a5be6f5683e9d9417f0220af
craftmallus <craftmallus@gmail.com>
2022-06-25 03:07:15 +0200

    add gblog tool script

---
+++ tools/gblog.sh
@@ -0,0 +1,31 @@
+#!/bin/mksh
+media=$1 gtext=$2 size=7
+get_res() {
+    [[ ! -e "$2" ]] && return
+    case "$1" in
+        width|height)
+            local p=$1
+            local res=$(ffprobe -v error -show_streams "$2" \
+                | sed -n "s/^$p=\(.*\)/\1/p")
+        ;;
+    esac
+    printf '%s' "$res"
+}
+
+w=$(get_res width "$media")
+cw=$((w-(w/10)))
+ps=$(bc <<< "$w/$size")
+font="@$HOME/.local/share/fonts/upright.otf"
+convert \
+	-background none -fill white -font "$font" \
+	-stroke black -strokewidth 10 \
+	-gravity center -pointsize $ps \
+	-size $((cw+10))x caption:"$gtext" "$media" \
+	+swap -compose over -composite \
+	"out-0.png"
+convert \
+	-background none -fill white -font "$font" \
+	-gravity center -pointsize $ps \
+	-size ${cw}x caption:"$gtext" "out-0.png" \
+	+swap -compose over -composite \
+	"out.png"
